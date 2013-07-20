4efd3efa21a7a65981559010a70dde71d40eb98a
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-07-20 04:02:45 -0700

    Upgraded script to work from the command line

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var fs = require('fs');
+#!/usr/local/bin/node
 
-fs.readFile('checking.qfx', 'utf8', function(err, data) {
+var fs = require('fs');
+var argv = require('optimist')
+    .usage('Usage: $0 --file [string]')
+    .demand(['file'])
+    .argv;
+
+
+fs.readFile(argv.file, 'utf8', function(err, data) {
 	if(err) throw err;
 	
 	var newData = data.valueOf();
 	
 	newData = newData.replace("<ORG>U.S. Bank<FID>1402</FI><INTU.BID>1402", "<ORG>AMEX<FID>3106</FI><INTU.BID>3106");
 	
-	fs.writeFile('checking.qbo', newData, function (err) {
+	fs.writeFile('download.qbo', newData, function (err) {
 	  if (err) throw err;
 	  console.log('It\'s saved!');
 	});
