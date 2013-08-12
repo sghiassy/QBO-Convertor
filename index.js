bd608de2f25fba9bec7e8ce32370b2a4330e4cde
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-08-12 11:33:19 -0700

    Updated logic to save converted file with same name of input file

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 #!/usr/local/bin/node
 
+//Usage Example: ./index.js --file personal.qfx
+
 var fs = require('fs');
 var argv = require('optimist')
     .usage('Usage: $0 --file [string]')
     .demand(['file'])
     .argv;
+var fileToConvert = argv.file;
 
 
-fs.readFile(argv.file, 'utf8', function(err, data) {
+fs.readFile(fileToConvert, 'utf8', function(err, data) {
 	if(err) throw err;
 	
 	var newData = data.valueOf();
 	
 	newData = newData.replace("<ORG>U.S. Bank<FID>1402</FI><INTU.BID>1402", "<ORG>AMEX<FID>3106</FI><INTU.BID>3106");
 	
-	fs.writeFile('download.qbo', newData, function (err) {
+	var newFileName = changeFileExtension(fileToConvert);
+	
+	fs.writeFile(newFileName, newData, function (err) {
 	  if (err) throw err;
 	  console.log('It\'s saved!');
 	});
 });
+
+function changeFileExtension(inputFileName) {
+	if(inputFileName.split('.').length >= 3 || typeof inputFileName !== "string") {
+		throw "Error 3XS: Improper filename argument";
+	}
+
+	var oldFileName = inputFileName.split('.')[0];
+	var newFileName = oldFileName + ".qbo";
+
+	return newFileName;
+}
