beb4619a0dfc67a44cc36fdbe0b6d33ec517fa3c
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2013-09-24 21:56:14 -0700

    Heroku-nizing the App

---
+++ server.js
@@ -0,0 +1,13 @@
+var ONEDAY = 86400000;
+
+var express = require("express");
+var app = express();
+app.use(express.logger());
+
+app.use('/', express.static(__dirname + '/build'), {maxAge: ONEDAY});
+
+var port = process.env.PORT || 5000;
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
