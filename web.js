e9bd3019e36e426c01e098b7f2640d3507298edf
Shaheen Ghiassy <shaheen.ghiassy@gmail.com>
2019-06-23 11:39:36 -0700

    Upgrade Express Dependency (#2)
    * Npm install
    
    * npm audit fix
    
    * Working
    
    * Upgrade express dependencies
    
    * Changed to use 1d syntax

--- web.js
+++ web.js
@@ -1,17 +1,19 @@
-var ONEDAY = 86400000;
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+const path = require('path')
+const serveStatic = require('serve-static')
 
-var express = require("express");
-var app = express();
-app.use(express.logger());
+app.use(logger('dev'))
 
-app.get('/test', function(request, response) {
-  response.send('Hello World from Heroku');
-});
+app.get('/test', (req, res) => {
+  res.send('Hello World from Heroku')
+})
 
-app.use('/', express.static(__dirname + '/build'), {maxAge: ONEDAY});
+app.use(serveStatic(path.join(__dirname, '/build'), {
+  maxAge: '1d'
+}))
 
-var port = process.env.PORT || 9000;
+const port = process.env.PORT || 9000;
 
-app.listen(port, function() {
-	console.log("Listening on " + port);
-});
+app.listen(port, () => console.log(`Listening on port ${port}!`))
